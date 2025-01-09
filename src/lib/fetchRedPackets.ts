384d9ae79e98b1d6d7ed4ba65004e62c7e748cfd
guanbinrui <guanbinrui@mask.io>
2025-01-09 22:04:42 +0800

    chore: query

--- src/lib/fetchRedPackets.ts
+++ src/lib/fetchRedPackets.ts
@@ -1,11 +1,21 @@
 import { getRpProgram } from "@/helpers/getRpProgram";
 import { web3 } from "@coral-xyz/anchor";
 
-export async function fetchRedPackets() {
+export async function fetchRedPackets(creator: web3.PublicKey) {
+  console.log("DEBUG: creator");
+  console.log({
+    creator: creator.toBase58(),
+  });
+
   const program = await getRpProgram();
 
-  const redPacket = await program.account.redPacket.fetch(
-    new web3.PublicKey("FHKgmXEUqf5j1Xc82kz5NpRHfScL4FsGFA93R8ZhtcTa"),
-  );
-  return [redPacket];
+  const redPackets = await program.account.redPacket.all([
+    {
+      memcmp: {
+        offset: 8, // Adjust the offset based on your account structure
+        bytes: creator.toBase58(),
+      },
+    },
+  ]);
+  return redPackets;
 }
